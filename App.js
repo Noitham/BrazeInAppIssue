abdcba809263465121ba229634b0af587259f2cd
Daniel Morales <dmoralesgonzalez97@gmail.com>
2024-10-03 12:46:49 +0200

    rename package name & adds StatusBar modification

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { useEffect } from "react";
+import { StyleSheet, Text, View, Button, StatusBar } from "react-native";
 import Braze from "@braze/react-native-sdk";
+import { useEffect, useState } from "react";
 
 const userId = "{YOUR_USER_ID}";
 
+/**
+ * Status bar modification.
+ * We seem to be experiencing the issue only when the status bar is translucent.
+ */
+StatusBar.setTranslucent(true);
+StatusBar.setBarStyle("dark-content", false);
+StatusBar.setBackgroundColor("deeppink");
+
 export default function App() {
+  const [userId, setUserId] = useState(null);
+
   useEffect(() => {
-    Braze.changeUser(userId);
-  }, []);
+    if (userId) {
+      Braze.changeUser(userId);
+    }
+  }, [userId]);
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
       <Text>Open up App.js to start working on your app!</Text>
+      <Button onPress={() => setUserId(USER_ID)} title="Set user ID" />
+      <Text>{userId ?? "No user ID set"}</Text>
     </View>
   );
 }
@@ -21,7 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "pink",
     alignItems: "center",
     justifyContent: "center",
   },
--- android/app/src/main/java/com/joejuice/joeloyalty/braze/MainApplication.java
+++ android/app/src/main/java/com/joejuice/joeloyalty/braze/MainApplication.java
@@ -1,4 +1,4 @@
-package com.joejuice.joeloyalty;
+package com.joejuice.joeloyalty.braze;
 
 import android.app.Application;
 import android.content.res.Configuration;
